e6b56bf6129e9b7ce2c4e69f757230e1cb48c522
Tyler Lynch <tylerlynch01@gmail.com>
2023-12-18 15:45:08 -0500

    update embed if api error

--- src/main.py
+++ src/main.py
@@ -35,47 +35,55 @@ def getRandomResponse():
     return random.choice(listOfResponses)
 
 async def update_minecraft_server_status(message):
-    # Get details of the mc server
-    request = Request(
-        url=f"https://api.mcsrvstat.us/bedrock/3/{MINECRAFT_ADDRESS}", 
-        headers={'User-Agent': 'Mozilla/5.0'}
-    )
-    response = urlopen(request)
-    data = json.load(response)
-    
-    online = data['online']
-    port = data['port']
-    name = data['motd']['clean'][0].strip()
-    version = data['version']
-    online_players = data['players']['online']
-    max_players = data['players']['max']
+    try:
+        request = Request(
+            url=f"https://api.mcsrvstat.us/bedrock/3/{MINECRAFT_ADDRESS}", 
+            headers={'User-Agent': 'Mozilla/5.0'}
+        )
+        response = urlopen(request)
+        data = json.load(response)
+        
+        online = data['online']
+        port = data['port']
+        name = data['motd']['clean'][0].strip()
+        version = data['version']
+        online_players = data['players']['online']
+        max_players = data['players']['max']
 
-    # Try to get the details of the server
-    if online:
-        description = 'Server is Live!'
-    else:
-        description = 'Server is Not Online'
+        embed=discord.Embed(title="Server Status", description=description, color=0x109319)
+        
+        embed.set_author(name="Minecraft Server", icon_url=MINECRAFT_BLOCK_IMAGE)
+        embed.set_thumbnail(url=MINECRAFT_BLOCK_IMAGE)
+        
+        # Get the time and add as footer
+        now = datetime.now()
+        current_time = now.strftime("%a %d %b %H:%M")
+        embed.set_footer(text=f"Last Updated: {current_time}")
 
-    # Create the initial embed object
-    embed=discord.Embed(title="Server Status", description=description, color=0x109319)
+        if online:
+            description = 'Server is Live!'
+        else:
+            description = 'Server is Not Online'
+        
+        embed.add_field(name="Name", value=name, inline=False)
+        embed.add_field(name="Address", value=MINECRAFT_ADDRESS, inline=True)
+        embed.add_field(name="Port", value=port, inline=True)
+        embed.add_field(name="Players", value=f"{online_players}/{max_players}", inline=False)
+        embed.add_field(name="Version", value=version, inline=True)
+    except Exception as ex:
+        embed=discord.Embed(title="Server Status", description=description, color=0x109319)
+        
+        embed.set_author(name="Minecraft Server", icon_url=MINECRAFT_BLOCK_IMAGE)
+        embed.set_thumbnail(url=MINECRAFT_BLOCK_IMAGE)
+        
+        # Get the time and add as footer
+        now = datetime.now()
+        current_time = now.strftime("%a %d %b %H:%M")
+        embed.set_footer(text=f"Last Updated: {current_time}")
 
-    # Add author, thumbnail, fields, and footer to the embed
-    embed.set_author(name="Minecraft Server", icon_url=MINECRAFT_BLOCK_IMAGE)
+        embed.add_field(name="Uh Oh!", value='Damn, the api must be broken again.', inline=False)
+        embed.add_field(name="Error", value=ex, inline=False)
 
-    embed.set_thumbnail(url=MINECRAFT_BLOCK_IMAGE)
-    
-    # Add the fields
-    embed.add_field(name="Name", value=name, inline=False)
-    embed.add_field(name="Address", value=MINECRAFT_ADDRESS, inline=True)
-    embed.add_field(name="Port", value=port, inline=True)
-    embed.add_field(name="Players", value=f"{online_players}/{max_players}", inline=False)
-    embed.add_field(name="Version", value=version, inline=True)
-
-    # Get the time and add as footer
-    now = datetime.now()
-    current_time = now.strftime("%a %d %b %H:%M")
-    embed.set_footer(text=f"Last Updated: {current_time}")
-    
     # Send the embed to the discord channel
     await message.edit(embed=embed)
 
