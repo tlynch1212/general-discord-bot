62dcd1a1ad03fa8bc4bf8acf29d874d614928634
Tyler Lynch <tylerlynch01@gmail.com>
2024-01-12 15:59:49 -0500

    Update chatbot.py

--- src/library/chatbot.py
+++ src/library/chatbot.py
@@ -1,6 +1,7 @@
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 from chatterbot.trainers import UbuntuCorpusTrainer
+import threading
 import spacy
 
 nlp = spacy.load("en_core_web_sm")
@@ -19,11 +20,20 @@ gynchChat = ChatBot(
         'chatterbot.logic.TimeLogicAdapter'
     ])
 
-# Create a new trainer for the chatbot
-trainer = UbuntuCorpusTrainer(gynchChat)
 
-# Train the chatbot based on the english corpus
-trainer.train()
+
+def trainChatBot():
+    # Create a new trainer for the chatbot
+    trainer = UbuntuCorpusTrainer(gynchChat)
+
+    # Train the chatbot based on the english corpus
+    trainer.train()
+
+trainingThread = threading.Thread(target=trainChatBot, name="Trainer")
+trainingThread.start()
 
 async def talkToGynch(message):
-    await message.channel.send(gynchChat.get_response(str.lower(message.content)))
+    if trainingThread.isAlive():
+        await message.channel.send('Sorry, im currently in training, it takes awhile to learn over 100 million conversations.')
+    else:
+        await message.channel.send(gynchChat.get_response(str.lower(message.content)))
